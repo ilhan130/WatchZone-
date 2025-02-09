721ca8b488df3d616b208b20ad208ac800cfbf42
ilhan130 <mrlotayt@gmail.com>
2025-02-09 07:13:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function downloadFile(fileName) {
+    const fileUrl = fileName; // File in the same directory
+    const anchor = document.createElement('a');
+    anchor.href = fileUrl;
+    anchor.download = fileName;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+}
